43fed8147589c6c15395c919e181b479853820d4
lestertai8 <lestertai2024@u.northwestern.edu>
2025-02-16 14:59:02 -0600

    added feedback for buttons

--- frontend/news-menu/src/components/StartPage/StartPage.jsx
+++ frontend/news-menu/src/components/StartPage/StartPage.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import "./StartPage.css";
 import api from "../../api.js";
 import Story from "../Story/Story.jsx";
+import Alert from '@mui/material/Alert';
 
 
 // tutorial on audio in react app
@@ -16,6 +17,7 @@ function StartPage() {
     const [topic, setTopic] = useState("");
     // const [text, setText] = useState("");
     const [articles, setArticles] = useState([]);
+    const [unfilledAlert, setUnfilledAlert] = useState(false);
     // const [summary, setSummary] = useState("");
     // const [quiz, setQuiz] = useState("");
 
@@ -45,23 +47,33 @@ function StartPage() {
 
     const handleSubmit = async () => {
         try {
-            // button begins to load
-            setButtonLoading(true);
-            setArticles([]);
-            const res = await api.post("/articles", {category: topic, time: time, persona: persona});
-            setArticles(res.data.news);
-            // there's now an array of articles... we can parse each article and get the text, author, etc.
-            // https://stackoverflow.com/questions/69318193/how-to-use-map-in-react-to-create-multiple-component
-
-
-
-            // setText("");
-            // setSummary("");
-            // const story = "Once there was a little piggy who lived in a house made of straw. One day, a big bad wolf came along and blew it down.";
-            // const story = await api.post("/articles", {time: time});
-            // setText(story.data.news.slice(0, time).join("\n\n\n")); // there is now an array of text...
-            // const response = await api.post("/summary", {persona: persona, text: text});
-            // setSummary(response.data.summary);
+            if (!time || !topic || !persona) {
+                setUnfilledAlert(true);
+                return;
+            }
+            else {
+                setUnfilledAlert(false);
+                // button begins to load
+                setButtonLoading(true);
+                setArticles([]);
+                const res = await api.post("/articles", {category: topic, time: time, persona: persona});
+                setArticles(res.data.news);
+                setTime(0);
+                setTopic("");
+                setPersona("");
+                // there's now an array of articles... we can parse each article and get the text, author, etc.
+                // https://stackoverflow.com/questions/69318193/how-to-use-map-in-react-to-create-multiple-component
+    
+    
+    
+                // setText("");
+                // setSummary("");
+                // const story = "Once there was a little piggy who lived in a house made of straw. One day, a big bad wolf came along and blew it down.";
+                // const story = await api.post("/articles", {time: time});
+                // setText(story.data.news.slice(0, time).join("\n\n\n")); // there is now an array of text...
+                // const response = await api.post("/summary", {persona: persona, text: text});
+                // setSummary(response.data.summary);
+            }
         } catch (error) {
             console.error("Error getting articles:", error);
         }
@@ -71,28 +83,49 @@ function StartPage() {
         <div className="start-page">
             <h2>How long will your meal be?</h2>
             <ButtonGroup variant="contained" aria-label="Basic button group">
-                <Button onClick={() => handleButtonTime(1)}>1 minute</Button>
-                <Button onClick={() => handleButtonTime(2)}>2 minutes</Button>
-                <Button onClick={() => handleButtonTime(3)}>3 minutes</Button>
+                <Button onClick={() => handleButtonTime(1)} style={{
+                    backgroundColor: time === 1 ? "#BE5103" : "#165fc7"
+                }}>1 minute</Button>
+                <Button onClick={() => handleButtonTime(2)} style={{
+                    backgroundColor: time === 2 ? "#BE5103" : "#165fc7"
+                }}>2 minutes</Button>
+                <Button onClick={() => handleButtonTime(3)} style={{
+                    backgroundColor: time === 3 ? "#BE5103" : "#165fc7"
+                }}>3 minutes</Button>
             </ButtonGroup>
             <h2>What cuisine are you interested in?</h2>
             <ButtonGroup variant="contained" aria-label="Basic button group">
-                <Button onClick={() => handleButtonTopic("tech")}>Tech</Button>
-                <Button onClick={() => handleButtonTopic("sports")}>Sports</Button>
-                <Button onClick={() => handleButtonTopic("food")}>Food</Button>
+                <Button onClick={() => handleButtonTopic("tech")} style={{
+                    backgroundColor: topic === "tech" ? "#BE5103" : "#165fc7"
+                }}>Tech</Button>
+                <Button onClick={() => handleButtonTopic("sports")} style={{
+                    backgroundColor: topic === "sports" ? "#BE5103" : "#165fc7"
+                }}>Sports</Button>
+                <Button onClick={() => handleButtonTopic("food")} style={{
+                    backgroundColor: topic === "food" ? "#BE5103" : "#165fc7"
+                }}>Food</Button>
             </ButtonGroup>
             <h2>Who will your server be?</h2>
             <ButtonGroup variant="contained" aria-label="Basic button group">
                 {/* Source: used ChatGPT to generate some examples of personas */}
-                <Button onClick={() => handleButtonPersona("Middle Schooler")}>Middle Schooler</Button>
-                <Button onClick={() => handleButtonPersona("William Shakespeare")}>William Shakespeare</Button>
-                <Button onClick={() => handleButtonPersona("Conspiracy Theorist")}>Conspiracy Theorist</Button>
-                <Button onClick={() => handleButtonPersona("Angry person")}>Sarcastic Person</Button>
+                <Button onClick={() => handleButtonPersona("Middle Schooler")} style={{
+                    backgroundColor: persona === "Middle Schooler" ? "#BE5103" : "#165fc7"
+                }}>Middle Schooler</Button>
+                <Button onClick={() => handleButtonPersona("William Shakespeare")} style={{
+                    backgroundColor: persona === "William Shakespeare" ? "#BE5103" : "#165fc7"
+                }}>William Shakespeare</Button>
+                <Button onClick={() => handleButtonPersona("Conspiracy Theorist")} style={{
+                    backgroundColor: persona === "Conspiracy Theorist" ? "#BE5103" : "#165fc7"
+                }}>Conspiracy Theorist</Button>
+                <Button onClick={() => handleButtonPersona("Angry person")} style={{
+                    backgroundColor: persona === "Angry person" ? "#BE5103" : "#165fc7"
+                }}>Sarcastic Person</Button>
             </ButtonGroup>
             <h2>Ready to order?</h2>
             {buttonLoading ? 
             <Button variant="contained" disabled className="loading-button">Loading...</Button> :
             <Button variant="contained" onClick={handleSubmit}>All ready!</Button>}
+            {unfilledAlert && <Alert severity="error" className="alert">Please select a time, news topic, and persona.</Alert>}
             {/* <h2>Original Text</h2>
             <h3>{text || "no text"}</h3>
             <h2>{persona || "no one selected yet"}</h2>
